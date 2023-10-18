2adddb0b2b28a3facc33804b04f465fb754d4f28
kyrie <cjybcjy@gmail.com>
2023-10-18 18:15:13 +0800

    lab2: add unused_procmem function in kernel/proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -658,3 +658,17 @@ procdump(void)
     printf("\n");
   }
 }
+
+void
+unused_procmem(void)
+{   struct proc *p;
+    uint64 count = 0;
+    for (p = proc; p < &proc[NPROC]; p++) {
+        acquire(&p->lock);
+        if (p->state == UNUSED) {
+            count++;
+        }
+        release(&p->lock);
+    }
+    return count;
+}
